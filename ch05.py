bdec26690ca04ba3f46b963db361dd2fe32f9f7b
unknown <mca@.(none)>
2009-01-02 02:38:57 -0500

    completed ch05

--- ch05.py
+++ ch05.py
@@ -33,3 +33,22 @@ d = counter()
 print d.count
 print c.count
 print counter.count
+
+class counter2:
+    __count = 0
+    def __init__(self):
+        pass #self.__count += 1
+    def Increment(self):
+        self.__count += 1
+        return self.__count
+        
+print counter2
+c = counter2()
+print c.Increment()
+d = counter2()
+print d.Increment()
+print c.Increment()
+print c.Increment()
+print c.Increment()
+print d.Increment()
+
